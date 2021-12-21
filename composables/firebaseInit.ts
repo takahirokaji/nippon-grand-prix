a8b5c322f871c642507019461dbba2d5ecb48d52
takahirokaji <tk69kj619@gmail.com>
2021-12-22 01:52:33 +0900

    first commit

---
+++ composables/firebaseInit.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB49juIjztu7Z93TlOr8KMUDqgzA7d2oLc",
+  authDomain: "nippon-grand-prix.firebaseapp.com",
+  projectId: "nippon-grand-prix",
+  storageBucket: "nippon-grand-prix.appspot.com",
+  messagingSenderId: "899607158950",
+  appId: "1:899607158950:web:3fb5d507d2678443005467",
+  measurementId: "G-JFW7RLVL08",
+};
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
