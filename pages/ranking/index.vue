0c6b429b3e4b76c25df5f248833d227bff096a2a
yamato-creator <dydydydydydy5656@gmail.com>
2021-12-27 06:31:44 +0900

    add:ranking

---
+++ pages/ranking/index.vue
@@ -0,0 +1,56 @@
+<script setup>
+import {
+  getQuestionAndAnswer,
+} from "../../composables/useFirestore";
+
+// const questions = await getQuestionAndAnswer();
+
+// console.log(questions)
+</script>
+
+    <!-- <ul v-for="question in questions" :key="question.id">
+      <li>
+        {{ questions }}
+      </li>
+    </ul> -->
+<template>
+  <img class="w-full pb-11 bg-black" src="~/assets/img/nippon-grand-prix_title_2.png"
+    alt="title" />
+
+  <div class="w-screen flex justify-center items-center bg-black">
+    <div class="container mx-auto max-w-xs rounded-lg overflow-hidden shadow-lg my-2">
+      <div class="relative mb-6">
+        <img class="w-full" src="~/assets/img/timeline.jpeg"
+            alt="Q&A" />
+      </div>
+      <div class="relative mb-6">
+      <img class="w-full" src="~/assets/img/timeline.jpeg"
+            alt="Q&A" />
+      </div>
+      <div class="relative mb-6">
+      <img class="w-full" src="~/assets/img/timeline.jpeg"
+            alt="Q&A" />
+      </div>
+      <div class="relative mb-6">
+          <img class="w-full" src="~/assets/img/timeline.jpeg"
+            alt="Q&A" />
+      </div>
+      <div class="relative mb-6">
+          <img class="w-full" src="~/assets/img/timeline.jpeg"
+            alt="Q&A" />
+      </div>
+      <div class="relative mb-6">
+          <img class="w-full" src="~/assets/img/timeline.jpeg"
+            alt="Q&A" />
+      </div>
+      <div class="relative mb-6">
+          <img class="w-full" src="~/assets/img/timeline.jpeg"
+            alt="Q&A" />
+      </div>
+      <div class="relative mb-6">
+          <img class="w-full" src="~/assets/img/timeline.jpeg"
+            alt="Q&A" />
+      </div>
+    </div>
+  </div>
+</template>
